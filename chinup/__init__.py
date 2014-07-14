e2495040277fafdac4c0e060517cf667baa27c02
Aron Griffis <aron@arongriffis.com>
2014-07-14 14:38:21 -0400

    Configure package-level logging to avoid warning.

--- chinup/__init__.py
+++ chinup/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
 try:
     from .allauth import *
 except ImportError:
@@ -7,3 +9,10 @@ from .exceptions import *
 
 
 __version__ = '0.1'
+
+
+# Configure logging to avoid warning.
+# https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library
+import logging
+if hasattr(logging, 'NullHandler'):
+    logging.getLogger('chinup').addHandler(logging.NullHandler())
